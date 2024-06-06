11ab70743834468efe591f83e2cdd2415dd1ae71
Patrick Kelley <PatrickO.kelley88@gmail.com>
2024-06-06 13:18:50 -0500

    Completed Exercise

--- app.js
+++ app.js
@@ -5,6 +5,49 @@ console.log(
 
 // Exercise 1
 
+let lastName;
+var age;
+
 // Exercise 2
 
+const firstName = "Patrick";
+lastName = "Kelley";
+age = 36;
+console.log(firstName, lastName, age);
+
 // Exercise 3
+
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensetive = true;
+console.log(language);
+console.log(createdYear);
+console.log(isCaseSensetive);
+
+// Exercise 4
+
+let price = 19.99;
+let isOnSale = true;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+console.log(price);
+console.log(isOnSale);
+console.log(salePercentage);
+console.log(stock);
+console.log(inStock);
+console.log(selectedSize);
+
+// Exercise 5
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+console.log(title);
+console.log(author);
+console.log(pageCount);
+console.log(bookmark);
+console.log(hasRead);
